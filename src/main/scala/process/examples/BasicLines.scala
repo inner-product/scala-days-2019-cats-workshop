bcfe1083d6d42171585d2a1ee3c931b9f83abf24
Noel Welsh <noel@untyped.com>
2019-06-09 13:40:24 +0100

    Nicer output from the starter code
    Tweak the starter code to produce more pleasing output (though many
    improvements remain).
    
    Produce many lines. Smooth out the lines. Add some comments to explain
    what is going on.

--- src/main/scala/process/examples/BasicLines.scala
+++ src/main/scala/process/examples/BasicLines.scala
@@ -1,6 +1,7 @@
 package process
 package examples
 
+import cats.implicits._
 import doodle.core._
 import doodle.syntax._
 import doodle.image._
@@ -8,24 +9,25 @@ import doodle.image.syntax._
 import doodle.java2d._
 import scala.util.Random
 
-object BasicLines extends App {
-  final case class State(location: Point, heading: Angle, path: List[PathElement]) {
+/** Simple process with no interaction between the elements. */
+object BasicProcess extends App {
+  final case class State(location: Point, heading: Angle, path: List[Point]) {
     def forward: State = {
       val newLocation = location + Vec.polar(10, heading)
       this.copy(
         location = newLocation,
-        path = PathElement.lineTo(newLocation) +: path
+        path = newLocation +: path
       )
     }
 
     def clockwise: State =
-      this.copy(heading = heading - 30.degrees)
+      this.copy(heading = heading - 15.degrees)
 
     def anticlockwise: State =
-      this.copy(heading = heading - 30.degrees)
+      this.copy(heading = heading - 15.degrees)
 
     def toImage: Image =
-      Image.openPath(path.reverse)
+      Image.interpolatingSpline(path)
   }
   object State {
     val initial = State(Point.zero, Angle.zero, List.empty)
@@ -33,25 +35,51 @@ object BasicLines extends App {
 
   type Event = Double
 
-  val basic =
+  /** The finite state machine defines how the state evolves over time. Tweaking
+    * the probabilities will arrive at different results. */
+  val fsm =
     Fsm[State,Event]{(state, choice) =>
       if(choice < 0.5) state.forward
       else if (choice < 0.75) state.clockwise
       else state.anticlockwise
     }
 
+  /** Execute one step of the FSM */
+  def step(state: State): State = {
+    val choice = Random.nextDouble()
+    fsm(state, choice)
+  }
+
+  /** Execute count steps of the FSM */
   def iterate(count: Int, state: State): State = {
     if(count == 0) state
     else {
-      val choice = Random.nextDouble()
-      iterate(count - 1, basic(state, choice))
+      iterate(count - 1, step(state))
     }
   }
 
-  val image =
-    iterate(100, State.initial).toImage.strokeWidth(3.0).strokeColor(Color.royalBlue.fadeOutBy(0.5.normalized))
+  def randomColor(): Color =
+    Color.hsla(
+      (Random.nextDouble() / 3.0 - 0.33).turns, // blues and reds
+      Random.nextDouble() / 2.0 + 0.4, // fairly saturated
+      Random.nextDouble() / 2.0 + 0.4, // fairly light
+      0.7 // Somewhat transparent
+    )
 
-  def go() = image.draw()
+  def squiggle(): Image =
+    iterate(100, State.initial).toImage.strokeWidth(3.0).strokeColor(randomColor())
+
+  def initialPosition(): Point = {
+    // Poisson disk sampling might be more attractive
+    Point(Random.nextGaussian() * 150, Random.nextGaussian() * 150)
+  }
+
+  def squiggles(): Image =
+    (for{
+       _ <- 0 to 100
+      } yield squiggle().at(initialPosition().toVec)).toList.allOn
+
+  def go() = squiggles().draw()
 
   go()
 }
